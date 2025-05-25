119bd6333b1bb2d89000a510bdd6569474f20051
Deneb <1728717924@qq.com>
2025-05-26 02:54:12 +0800

    2-2 init

---
+++ src/test/java/org/example/wikitest/WikiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wikitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
