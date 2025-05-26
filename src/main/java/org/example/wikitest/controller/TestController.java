9595817ff99485e399483f860dc2b03d9c323c68
Deneb <1728717924@qq.com>
2025-05-26 14:52:34 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/org/example/wikitest/controller/TestController.java
+++ src/main/java/org/example/wikitest/controller/TestController.java
@@ -1,12 +1,20 @@
 package org.example.wikitest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "HELLO WORLD!!!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "HELLO WORLD!!POST!"+name;
     }
 }
