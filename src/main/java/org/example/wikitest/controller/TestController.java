32f00918883a68ad5765f4715b52233a3e094757
Deneb <1728717924@qq.com>
2025-05-26 15:11:04 +0800

    2-9 增加了自定义配置项的示例

--- src/main/java/org/example/wikitest/controller/TestController.java
+++ src/main/java/org/example/wikitest/controller/TestController.java
@@ -1,16 +1,19 @@
 package org.example.wikitest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
     @GetMapping("/hello")
     public String hello() {
-        return "HELLO WORLD!!!";
+        return "HELLO WORLD!!!"+testHello;
     }
 
     @PostMapping("/hello/post")
