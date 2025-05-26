fd7aea2f6efa4fe7ba768a9daf3a6f3984406e90
Deneb <1728717924@qq.com>
2025-05-26 14:09:44 +0800

    2-5 修改启动文案

--- src/main/java/org/example/wikitest/WikiTestApplication.java
+++ src/main/java/org/example/wikitest/WikiTestApplication.java
@@ -1,13 +1,20 @@
 package org.example.wikitest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiTestApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(WikiTestApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(WikiTestApplication.class, args);
-	}
+		SpringApplication app = new SpringApplication(WikiTestApplication.class);
+		Environment env = app.run(args).getEnvironment();
 
+		LOG.info("启动成功！！");
+		LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
 }
