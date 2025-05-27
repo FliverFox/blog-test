3df41c22ee6366dcdf60643159fbf05327d2f362
Deneb <1728717924@qq.com>
2025-05-27 15:39:11 +0800

    3-1 增加了all.sql脚本

---
+++  doc/all.sql
@@ -0,0 +1,12 @@
+-- 删除旧表 (存在时)
+DROP TABLE IF EXISTS `test`;
+
+-- 创建新表（带完整注释）
+CREATE TABLE `test` (
+                        `id` BIGINT NOT NULL COMMENT '主键ID',
+                        `name` VARCHAR(50) COMMENT '用户名称',
+                        `password` VARCHAR(50) COMMENT '用户密码',
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB
+ DEFAULT CHARSET=utf8mb4
+ COMMENT='测试用基础表';
