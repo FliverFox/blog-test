3d4f580cad7babd2efa9382cd17ff1afdad4b153
Deneb <1728717924@qq.com>
2025-05-29 18:27:00 +0800

    3-5 集成Mybatis generator，生成test表代码

--- src/main/java/org/example/wikitest/controller/DemoController.java
+++ src/main/java/org/example/wikitest/controller/DemoController.java
@@ -1,16 +1,26 @@
 package org.example.wikitest.controller;
 
 import jakarta.annotation.Resource;
+import org.example.wikitest.domain.Demo;
 import org.example.wikitest.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
+@RequestMapping("/demo")
 public class DemoController {
 
     @Resource
     private DemoService demoService;
 
+    @GetMapping("/list")
+    public List<Demo> list() {
+        return demoService.list();
+    }
 
 
 
