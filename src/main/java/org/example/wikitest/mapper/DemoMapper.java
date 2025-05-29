4fba826016c8a6ae9dc40dba56ba93c345cfc932
Deneb <1728717924@qq.com>
2025-05-29 18:06:09 +0800

    3-5 集成Mybatis generator，生成test表代码

---
+++ src/main/java/org/example/wikitest/mapper/DemoMapper.java
@@ -0,0 +1,31 @@
+package org.example.wikitest.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.example.wikitest.domain.Demo;
+import org.example.wikitest.domain.DemoExample;
+
+import java.util.List;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    Demo selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByPrimaryKeySelective(Demo record);
+
+    int updateByPrimaryKey(Demo record);
+}
