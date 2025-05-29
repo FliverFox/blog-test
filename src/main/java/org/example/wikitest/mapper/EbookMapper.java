1d7aac73bf4041f4d102bfbdf3a3fa8484cce4c3
Deneb <1728717924@qq.com>
2025-05-29 18:45:53 +0800

    3-6 新增ebook表，生成持久层代码

---
+++ src/main/java/org/example/wikitest/mapper/EbookMapper.java
@@ -0,0 +1,31 @@
+package org.example.wikitest.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.example.wikitest.domain.Ebook;
+import org.example.wikitest.domain.EbookExample;
+
+import java.util.List;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+}
